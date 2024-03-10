fa104f9d159865bf2eadae37fead4fd69f901955
WongCSheng <w.chongsheng@digipen.edu>
2024-03-10 20:12:01 +0800

    Some changes to audio fading

--- ManagedScripts/TempEndScene.cs
+++ ManagedScripts/TempEndScene.cs
@@ -12,12 +12,13 @@ public class TempEndScene : Script
     {
         //audio = new AudioComponent();
         audio = gameObject.GetComponent<AudioComponent>();
-        audio.play(monsterSFX); audio.FadeOut(4, monsterSFX);
+        audio.play(monsterSFX);
+        audio.FadeOut(3, monsterSFX);
         audio.FadeIn(7, runningSFX);
     }
     public override void Start()
     {
-        
+
     }
     public override void Update()
     {
